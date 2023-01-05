b7f7c131011a965acce52f87aa491b6ee088aad8
Elbavary <khaledgamel2012@gmail.com>
2023-01-05 06:19:18 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Footer, Header } from './components';
+
+const App = () => {
+  return (
+    <>
+      <main>
+        <h1>Welcome To My E-Commerce</h1>
+      </main>
+    </>
+  );
+};
+
+export default App;
