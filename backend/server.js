a14de3b14ce5ebeb9778dab489d383eedf70f0c0
unknown <khaledgamel2012@gmail.com>
2023-03-11 19:31:06 +0200

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import morgan from 'morgan';
+import helmet from 'helmet';
+import compression from 'compression';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
@@ -21,6 +23,8 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+app.use(helmet());
+app.use(compression());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -34,17 +38,17 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   });
+// } else {
+app.get('/', (req, res) => {
+  res.send('API is running....');
+});
+// }
 
 app.use(notFound);
 app.use(errorHandler);
